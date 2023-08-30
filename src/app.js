18fddb49d9518f4e75ee289594c0ca6dd546b57f
mikemma <micaelafcavallero@gmail.com>
2023-08-30 20:36:07 -0300

    websockets

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 import express from "express";
 import productRouter from "./routers/products.router.js";
 import cartRouter from "./routers/carts.router.js";
+import { Server } from "socket.io";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./public"));
+app.use(express.static("./src/public"));
 
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 
-app.listen(8080, () => console.log("Server up"));
+const httpServer = app.listen(8080, () => console.log("Server Up!"));
+const socketServer = new Server(httpServer);
+
+let log = [];
+
+socketServer.on("connection", (socketClient) => {
+  console.log(`Nuevo cliente conectado: ${socketClient.id}`);
+  socketClient.emit("history", log);
+  socketClient.on("message", (data) => {
+    // log.push(data)
+    log.push({ userId: socketClient.id, message: data });
+    socketServer.emit("history", log);
+  });
+});
